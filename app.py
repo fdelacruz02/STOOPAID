9068a29956a7a1910cf92b4da52991b9a8767abd
SWPNL <sd2269@njit.edu>
2023-11-13 15:44:41 -0500

    OpenAI import fixed

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.llms import OpenAI
+#import OpenAI 
 from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
 import os
